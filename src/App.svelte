f677df98cfa133c54382de248440b5ae74a8ec8f
Brandon <bmcomis2018@gmail.com>
2022-09-18 01:26:36 -0500

    Added some CSS

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,14 @@
 <script>
 	let name = "";
+	let notes = [
+		{"value": "Watch the entire Breaking Bad TV Series"},
+		{"value": "Watch the entire Better Call Saul TV Series"},
+		{"value": "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP"},
+		{"value": "Consume dihydrogen monoxide"},
+		{"value": "Sleep"},
+		{"value": "I don't know anymore"},
+		{"value": "I wholeheartedly agree with your opinion"}
+	];
 
 </script>
 
@@ -18,27 +27,60 @@
 		<button>Send</button>
 	</label>
 
-	<h2>EEEE</h2>
+	<h2>Current Notes</h2>
+	
+	<div class="note-container">
+	{#each notes as note}
+		<div class="note">
+			{note.value}
+			<button>
+				Delete
+			</button>
+		</div>
+	{/each}
+	</div>
+
 </main>
 
 <style>
 	main {
-		text-align: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 		padding: 1em;
 	}
 
-	h1 {
+	h1, h2 {
 		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
 		font-weight: 100;
 	}
 
+	h1 {
+		font-size: 4em;
+	}
+
 	h2 {
-		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 2em;
-		font-weight: 100;
+	}
+
+	.note {
+		border: 1px solid;
+		border-radius: 5px;
+		box-shadow: 0px 0px 3px black;
+		padding: 1em 2em;
+
+		min-width: 200px;
+		width: max-content;
+		max-width: 35vw;
+
+		overflow-wrap: break-word;
+	}
+
+	.note-container {
+		display: grid;
+		grid-template-columns: 1fr;
+		gap: 15px;
 	}
 
 	@media (min-width: 640px) {
